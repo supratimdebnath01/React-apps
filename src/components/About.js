b0fb9ff9bf97d0fdcc21f7277d5cd0e396d74c31
Supratim Debnath <supratimdebnath01@gmail.com>
2022-09-17 08:28:37 +0530

    No information from the developer

--- src/components/About.js
+++ src/components/About.js
@@ -35,7 +35,7 @@ export default function About() {
             <div className="accordion-item">
                 <h2 className="accordion-header" id="headingOne">
                 <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne" style={myStyle}>
-                    Accordion Item #1
+                    Accordion Item 1
                 </button>
                 </h2>
                 <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
@@ -47,7 +47,7 @@ export default function About() {
             <div className="accordion-item">
                 <h2 className="accordion-header" id="headingTwo">
                 <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo" style={myStyle}>
-                    Accordion Item #2
+                    Accordion Item 2
                 </button>
                 </h2>
                 <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
@@ -59,7 +59,7 @@ export default function About() {
             <div className="accordion-item">
                 <h2 className="accordion-header" id="headingThree">
                 <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree" style={myStyle}>
-                    Accordion Item #3
+                    Accordion Item 3
                 </button>
                 </h2>
                 <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -20,6 +20,19 @@ export default function TextForm(props) {
         setText(event.target.value);
     }
 
+    const handleCopy = () => {
+        let text = document.getElementById("myBox");
+        text.select();
+        navigator.clipboard.writeText(text.value);
+    }
+
+    const handleExtraSpaces = () => {
+      let newText = text.split(/[ ]+/)
+      setText(newText.join(" "))
+    }
+
+ 
+
  const [text, setText] = useState('');
  
   return (
@@ -32,6 +45,9 @@ export default function TextForm(props) {
           <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
           <button className="btn btn-primary mx-4" onClick={handleLoClick}>Convert to Lowercase</button>
           <button className="btn btn-primary mx-4" onClick={handleClearClick}>Clear all</button>
+          <button className="btn btn-primary mx-4" onClick={handleCopy}>Copy Text</button>
+          <button className="btn btn-primary mx-4" onClick={handleExtraSpaces}>Remove Extra Spaces</button>   
+ 
     </div>
     <div className="container my-3">
           <h2>Your text summery</h2>
